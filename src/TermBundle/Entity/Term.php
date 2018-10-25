78b147dd84b1cdc12b64a5db4f18fb03221b440c
Roman <romaaverkin@gmail.com>
2018-10-25 13:16:39 +0700

    Сгенерировал сущность для Term.php

--- src/TermBundle/Entity/Term.php
+++ src/TermBundle/Entity/Term.php
@@ -36,4 +36,86 @@ class Term
      */
     private $created;
 
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Term
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Term
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Term
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
 }
---
+++ src/TermBundle/Entity/Term.php~
@@ -0,0 +1,39 @@
+<?php
+
+namespace TermBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Page
+ * @package PageBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="term")
+ */
+class Term
+{
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", length=255)
+     */
+    private $description;
+
+
+    /**
+     * @ORM\Column(type="datetime", length=255)
+     */
+    private $created;
+
+}
